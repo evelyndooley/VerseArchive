a2d6cd36f6bf3b436328a828abbf61ed4223cfaf
Nate <nate@natedooley.com>
2017-10-28 09:52:58 -0400

    Fixed script

--- website/assets/js/talkToJava.js
+++ website/assets/js/talkToJava.js
@@ -3,7 +3,7 @@
 // This is a program that submits AJAX requests to a Java Web Servlet
 // Dependencies: JavaServlet (NameTBD), JQuery, index.html
 $(document).on("click", "#submit", function(){ // JQuery DOM Event Listener
-	$.get("/RESTGET", function(data){ // AJAX get request
+	$.get("http://natedooley.com:8080/GiveItAString", function(data){ // AJAX get request
 		$("#submit").text(data); //data holds the returned String,
 	});// Which replaces the contents of the text field
 }
